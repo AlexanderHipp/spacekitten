177eb1c7fdca4455566471c5ce0ec189722aa16c
Alexander Hipp <alexander.hipp@sportpursuit.co.uk>
2016-06-21 22:05:47 +0200

    current stand

---
+++ Spacekitten/GameOverScene.swift
@@ -0,0 +1,44 @@
+//
+//  GameOverScene.swift
+//  SpriteKitEasyGame
+//
+//  Created by Alexander Hipp on 19/06/16.
+//  Copyright © 2016 LonelyGoldfish. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    init(size: CGSize, won:Bool) {
+        
+        super.init(size: size)
+        
+        backgroundColor = SKColor.whiteColor()
+        
+        let message = won ? "You Won!" : "Game over"
+        
+        let label = SKLabelNode(fontNamed: "Arial")
+        label.text = message
+        label.fontSize = 40
+        label.fontColor = SKColor.blackColor()
+        label.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(label)
+        
+        runAction(SKAction.sequence([
+            SKAction.waitForDuration(3.0),
+            SKAction.runBlock() {
+                
+                let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+                let scene = GameScene(size: size)
+                self.view?.presentScene(scene, transition:reveal)
+            }
+            ]))
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
