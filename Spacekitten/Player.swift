0d6b6eafaec98c0b3470758a755a190c7d2cce28
Alexander Hipp <alexander.hipp@sportpursuit.co.uk>
2016-07-25 21:37:17 +0200

    made the class

---
+++ Spacekitten/Player.swift
@@ -0,0 +1,54 @@
+//
+//  Player.swift
+//  Spacekitten
+//
+//  Created by Alexander Hipp on 25/07/16.
+//  Copyright © 2016 LonelyGoldfish. All rights reserved.
+//
+
+import SpriteKit
+
+
+class Player: SKNode {
+    
+    
+    let player = SKSpriteNode()
+    var playerSize = 60
+    let textureAtlas:SKTextureAtlas = SKTextureAtlas(named: "sprites.atlas")
+    
+    func definePlayer(sizeScreen: CGSize) {
+        player.texture = textureAtlas.textureNamed("blue")
+        player.size = CGSize(width: playerSize, height: playerSize)
+        player.zPosition = 13
+        player.position = positionPlayer(sizeScreen)
+        addChild(player)
+    }
+    
+    func updatePlayerPhysics() {
+        player.physicsBody = SKPhysicsBody(rectangleOfSize: player.size)
+        player.physicsBody?.dynamic = false
+        player.physicsBody?.categoryBitMask = PhysicsCategory.Player
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.Enemy
+        player.physicsBody?.collisionBitMask = PhysicsCategory.None
+    }
+    
+    func widthOfPlayer() -> CGFloat {
+        return player.size.width
+    }
+    
+    func heightOfPlayer() -> CGFloat {
+        return player.size.height
+    }
+    
+    func growPlayerWhenHit() {        
+        playerSize += 20
+        player.size = CGSize(width: playerSize, height: playerSize)
+    }
+    
+    func positionPlayer(sizeScreen: CGSize) -> CGPoint {
+        return CGPoint(x: sizeScreen.width * 0.5, y: sizeScreen.height * 0.5)
+    }
+    
+    
+    
+}
