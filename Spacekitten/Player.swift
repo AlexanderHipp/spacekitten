f669565054a4882dd36aa4de25717b508c006ee4
Alexander Hipp <alexander.hipp@sportpursuit.co.uk>
2016-10-16 20:18:43 +0200

    switched to bear head and made mouth to goal

--- Spacekitten/Player.swift
+++ Spacekitten/Player.swift
@@ -15,33 +15,48 @@ class Player: SKNode {
     
     let playerFace = SKSpriteNode()
     let playerHead = SKSpriteNode()
+    let playerMouth = SKSpriteNode()
     
-    var playerSize = 100
+    var playerSize = 150
+    var playerInnerSize = 150
+    
+    
     let textureAtlas:SKTextureAtlas = SKTextureAtlas(named: "sprites.atlas")
     
-    let imageRalphHead = "Ralph-head"
-    let imageRalphFace = "Ralph-face"
+    let imageHead = "head"
+    let imageFace = "face"
+    let imageMouth = "mouth"
     
     func definePlayer(sizeScreen: CGSize) {
         
+        playerSize = Int(sizeScreen.width) / 3
+        playerInnerSize = playerSize
+        
         // Head
-        playerHead.texture = textureAtlas.textureNamed(imageRalphHead)        
+        playerHead.texture = textureAtlas.textureNamed(imageHead)
         playerHead.size = CGSize(width: playerSize, height: playerSize)
         playerHead.zPosition = 12
         playerHead.position = positionPlayer(sizeScreen)
         
         // Face
-        playerFace.texture = textureAtlas.textureNamed(imageRalphFace)
+        playerFace.texture = textureAtlas.textureNamed(imageFace)
         playerFace.size = CGSize(width: playerSize, height: playerSize)
         playerFace.zPosition = 13
         playerFace.position = positionPlayer(sizeScreen)
         
+        // Mouth
+        playerMouth.texture = textureAtlas.textureNamed(imageMouth)
+        playerMouth.size = CGSize(width: playerSize, height: playerSize)
+        playerMouth.zPosition = 14
+        playerMouth.position = positionPlayer(sizeScreen)
+        
         addChild(playerFace)
         addChild(playerHead)
+        addChild(playerMouth)
     }
     
     func updatePlayerPhysics() {        
-        playerHead.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: self.imageRalphHead), size: playerHead.size)
+        playerHead.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: self.imageMouth), size: playerMouth.size)
         playerHead.physicsBody?.dynamic = false
         playerHead.physicsBody?.categoryBitMask = PhysicsCategory.Player
         playerHead.physicsBody?.contactTestBitMask = PhysicsCategory.Enemy
@@ -59,9 +74,16 @@ class Player: SKNode {
     func growPlayerWhenHit(damage: Int, sizeScreen: CGSize) {
         // How much player grows when he gets hit
         
+        let fullDamage = damage
+        let smallDamage = damage / 2
+        
         if playerSize <= Int(sizeScreen.width) {
-            playerSize += damage
+            
+            playerSize += fullDamage
+            playerInnerSize += smallDamage
             playerHead.size = CGSize(width: playerSize, height: playerSize)
+            playerFace.size = CGSize(width: playerInnerSize, height: playerInnerSize)
+            playerMouth.size = CGSize(width: playerInnerSize, height: playerInnerSize)
         }
     }
     
